a93575909fc1017570c0118e510f38fcea778ae5
pedro cuasqui <pedro.cuasqui@gmail.com>
2019-01-28 18:50:52 -0500

    se crea la accion independiente administrar-contenidos.js con el comando sails generate action

---
+++ api/controllers/administrador/administrar-contenidos.js
@@ -0,0 +1,28 @@
+module.exports = {
+
+
+  friendlyName: 'Administrar contenidos',
+
+
+  description: '',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs) {
+
+    // All done.
+    return;
+
+  }
+
+
+};

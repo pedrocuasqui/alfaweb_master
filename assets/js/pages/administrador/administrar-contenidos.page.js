0a4c4c5cde81f21feb0af52e7a2fece0beeb1e52
pedro cuasqui <pedro.cuasqui@gmail.com>
2019-01-31 12:22:39 -0500

    Se configura .eslintrc para que admita como variable global a parasails

--- assets/js/pages/administrador/administrar-contenidos.page.js
+++ assets/js/pages/administrador/administrar-contenidos.page.js
@@ -1,45 +1,45 @@
 parasails.registerPage('administrar-contenidos', {
-    //  ╦╔╗╔╦╔╦╗╦╔═╗╦    ╔═╗╔╦╗╔═╗╔╦╗╔═╗
-    //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
-    //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
-    data: {
-      valorPrueba: 'valores de prueba',
-      nombreModulo:'',
-      contenidoNuevo:{
-        nombreModulo:'',
-        descripcion:'modulo nuevo'
-      },
-      contenidosFront:[],
+  //  ╦╔╗╔╦╔╦╗╦╔═╗╦    ╔═╗╔╦╗╔═╗╔╦╗╔═╗
+  //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
+  //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
+  data: {
+    valorPrueba: 'valores de prueba',
+    nombreModulo: '',
+    contenidoNuevo: {
+      nombreModulo: '',
+      descripcion: 'modulo nuevo'
     },
-  
-    //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
-    //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
-    //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
-    beforeMount: function() {
-      // Vincula cualquier dato inicial desde el servidor en las páginas ejs exposeLocalsToBrowser()
-      _.extend(this, SAILS_LOCALS);
-      // contenidosFront.push(this.contenidos);
-    },
-    mounted: async function(){
+    contenidosFront: [],
+  },
 
-    },
-  
-    //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
-    //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
-    //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
-    methods: {
-      /* agregarModulo: function(){
-        this.contenidoNuevo.nombreModulo=this.nombreModulo;
-          this.contenidosFront.push(this.contenidoNuevo);
-          this.contenidoNuevo={
-            nombreModulo:'',
-            descripcion:'modulo nuevo'
-          };
-      }, */
-      clickSeleccionarModulo: function( nombreContenido){
-        console.log('se ha seleccionado el elemento: '+ nombreContenido);
-      }
-      
+  //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
+  //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
+  //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
+  beforeMount: function () {
+    // Vincula cualquier dato inicial desde el servidor en las páginas ejs exposeLocalsToBrowser()
+    _.extend(this, SAILS_LOCALS);
+    // contenidosFront.push(this.contenidos);
+
+  },
+  mounted: async function () {
+
+  },
+
+  //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+  //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
+  //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
+  methods: {
+    /* agregarModulo: function(){
+      this.contenidoNuevo.nombreModulo=this.nombreModulo;
+        this.contenidosFront.push(this.contenidoNuevo);
+        this.contenidoNuevo={
+          nombreModulo:'',
+          descripcion:'modulo nuevo'
+        };
+    }, */
+    clickSeleccionarModulo: function (nombreContenido) {
+      console.log('se ha seleccionado el elemento: ' + nombreContenido);
     }
-  });
-  
+
+  }
+});

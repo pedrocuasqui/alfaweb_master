f2b8db58f0234021331cd691685d0a01a6fbcc16
pedro cuasqui <pedro.cuasqui@gmail.com>
2019-02-13 22:55:55 -0500

    se insertan los elementos g de svg con el tooltip y se usa el evento @mouseover de vue para mostrar el tooltip

--- assets/js/pages/estudiante/mouse-sobre-imagen.page.js
+++ assets/js/pages/estudiante/mouse-sobre-imagen.page.js
@@ -3,17 +3,18 @@ parasails.registerPage('mouse-sobre-imagen', {
   //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
   //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
   data: {
-    //…
+    mouseX:800,
+    mouseY:600,
   },
 
   //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
   //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
   //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
-  beforeMount: function() {
+  beforeMount: function () {
     // Attach any initial data from the server.
     _.extend(this, SAILS_LOCALS);
   },
-  mounted: async function() {
+  mounted: async function () {
     //…
   },
 
@@ -21,17 +22,37 @@ parasails.registerPage('mouse-sobre-imagen', {
   //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
   //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
   methods: {
-    mouseOverMonitor(){
-      // alert('Seleccion Monitor');
+    mouseOverMonitor(event) {
+      // clientX/Y gives the coordinates relative to the viewport in CSS pixels.
+      console.log("x: " + event.clientX + " y: " + event.clientY)
+      
+      var svg = document.getElementById("lienzo-svg");
+      let bound = svg.getBoundingClientRect();
+      this.mouseX = event.clientX - bound.left;
+      this.mouseY = event.clientY - bound.top;
+      // $("#lienzo-svg").css({'transform' : 'translate(' +  this.mouseX +', ' + this.mouseY + ')'});
+      // console.log("x relativo: "+ x+ " y relativo: " + y)
+       var toolTip= document.getElementById("toolMonitor") ; 
+      toolTip.setAttribute("transform", "translate("
+            + this.mouseX  + "," 
+            + this.mouseY + ")"); 
+      console.log("x relativo: " + this.mouseX + " y relativo: " + this.mouseY);
     },
-    mouseOverCpu(){
+    mouseOverCpu() {
       // alert('Seleccion Cpu');
     },
-    mouseOverTeclado(){
+    mouseOverTeclado() {
       // alert('Seleccion Teclado');
     },
-    mouseOverMouse(){
+    mouseOverMouse() {
       // alert('Seleccion Mouse');
+    }
+  },
+  computed: {
+    styleToolTip() {
+      let estilo={transform: 'translate('+ this.mouseX+','+this.mouseY+')'};
+      console.log(estilo);
+      return estilo
     }
   }
 });

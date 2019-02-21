bed82ec36d76d874b6877bb0ddee1c7538cee6a2
pedro cuasqui <pedro.cuasqui@gmail.com>
2019-02-20 19:44:02 -0500

    conteo de aciertos

--- assets/js/pages/estudiante/m1-evaluacion.page.js
+++ assets/js/pages/estudiante/m1-evaluacion.page.js
@@ -9,6 +9,11 @@ parasails.registerPage('m1-evaluacion', {
     elementoTurno: '',
     timer: null,
     tiempoSegundos: 5,
+    contadorTimer:0,
+    idElementoSeleccionado:null,
+    esCorrecto:false,
+    conteoAciertos: 0,
+    actividadFinaliza:false
 
   },
 
@@ -25,8 +30,12 @@ parasails.registerPage('m1-evaluacion', {
     for (i = 0; i < objetosg.length; i++) {
       this.elementos.push(objetosg[i].getAttribute('id'))
     }
-    console.log(this.elementos.toString())
-    this.iniciaConteo();
+    console.log(this.elementos.toString());
+    this.elementoTurno=this.elementos[this.contadorTimer];
+    setTimeout(this.iniciaConteo,1000);
+  },
+  updated:function(){
+// console.log('actualizado');
   },
 
   //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
@@ -35,37 +44,54 @@ parasails.registerPage('m1-evaluacion', {
   methods: {
     iniciaConteo() {
       this.timer = setInterval(() => this.cuentaRegresiva(), 1000);
-
     },
     cuentaRegresiva() {
-      let conteoElementos = this.elementos.length;
-      do{
-        this.elementoTurno = this.elementos[conteoElementos-1];
-      }
-      while (conteoElementos >= 1) {
-        if (this.tiempoSegundos >= 1) {
-          this.tiempoSegundos--;
-        } else {
-          this.tiempoSegundos = 5;
-          this.elementoTurno = this.elementos[conteoElementos];
+// Si el contador aun no ha llegado a cero, continua restando un numero cada segundo
+      if (this.tiempoSegundos >= 1) {
+        this.tiempoSegundos--;
+      } else {
+        // si el contador de elementos ha llegado a su fin, se detiene la funcion setInterval
+        if(this.contadorTimer==this.elementos.length-1){
+          clearInterval(this.timer);
+          this.tiempoSegundos=0;
+          this.elementoTurno='';
+          this.actividadFinaliza=true;
         }
-        conteoElementos--;
+        // caso contrario se aumenta el contador de elementos y se obtiene un elemento "g" actual
+        else{
+          this.contadorTimer++;
+          this.tiempoSegundos = 5;
+          this.elementoTurno=this.elementos[this.contadorTimer];
+        }
+        
       }
-      // clearInterval(this.timer);
-      //     this.regresaLaPagina()
-    },
-    regresaLaPagina() {
-      console.log('regresa a la página anterior')
+
     },
     mouseClickPc(event) {
+      // event.target funciona tal como document.getElementById() 
+      // fuente: https://stackoverflow.com/questions/7723188/what-properties-can-i-use-with-event-target
       let elementoSeleccionado = event.target;
+      // si el elemento seleccionado es un elemento "path" entonces selecciona al objeto padre
+      if(elementoSeleccionado){
+        this.idElementoSeleccionado=elementoSeleccionado.parentNode;
+        // caso contrario selecciona al elemento "event"
+      }else{
+        this.idElementoSeleccionado=elementoSeleccionado;
+      }
+      if(this.idElementoSeleccionado.getAttribute('id') == this.elementoTurno){
+        this.esCorrecto=true;
+        this.conteoAciertos++;
+        this.tiempoSegundos = 0;
+        this.idElementoSeleccionado.setAttribute("style", "fill: chartreuse;")
+        document.getElementById(this.idElementoSeleccionado.getAttribute('id')).removeAttribute('onmousedown');
+      }else{
+        this.tiempoSegundos = 0;
+        this.idElementoSeleccionado.setAttribute("style", "fill: red;")
+      }
 
     }
   },
   computed: {
-    // objetoDeTurno() {
-    //   let elem = 
-    //   return
-    // }
+
   }
 });

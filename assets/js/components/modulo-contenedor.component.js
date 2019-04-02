73e5581bf56af37eb9dc9c8074c80e2c049340b6
pedro cuasqui <pedro.cuasqui@gmail.com>
2019-04-01 19:35:54 -0500

    se inserta la columna central y la lateral derecha en una sola fila para ampliar el ancho de la barra lateral derecha

--- assets/js/components/modulo-contenedor.component.js
+++ assets/js/components/modulo-contenedor.component.js
@@ -37,45 +37,47 @@ parasails.registerComponent('modulo-contenedor', {
         <div class="col-sm-2 col-izquierda">
             <modulo-side-var-menu></modulo-side-var-menu>
         </div>
+        <div class="col-sm-10" id="columna-contenido-lateral">
+            <div class="row fila-principal">
+                <!-- Columna central -->
+                <div class="col-sm-10 col-central">
+                    <div class="row">
+                        <div class="navegacion-siguiente">
+                            <a :href="navegarSiguiente"><i class="fas fa-arrow-alt-circle-right fas-3x"></i> </a>
+                        </div>
+                        <div class="navegacion-atras">
+                            <a :href="navegarAtras"> <i class="fas fa-arrow-alt-circle-left fas-3x"></i> </a>
+                        </div>
+                        <div class="col" id="titulo-modulo">
+                            <h2>"{{tituloContenido}}"</h2>
+                        </div>
+                    </div>
 
-        <!-- Columna central -->
-        <div class="col-sm-8 col-central">
-            <div class="row">
-    
-            <div class="navegacion-siguiente">
-                <a :href="navegarSiguiente"><i class="fas fa-arrow-alt-circle-right fas-3x"></i> </a>
-            </div>
-            <div class="navegacion-atras">
-                <a :href="navegarAtras"> <i class="fas fa-arrow-alt-circle-left fas-3x"></i> </a>
-            </div>
-                <div class="col" id="titulo-modulo">
-                    <h2>"{{tituloContenido}}"</h2>
+                    <div class="row">
+                        <!-- IMAGEN SVG -->
+                        <slot></slot>
+                    </div>
+
+
+                    <div class="row">
+                        <div class="col-sm-1" id="avatar">
+                            <img src="/images/myAvatar_adultoMayor.png" alt="Avatar adulto mayor">
+                        </div>
+
+                        <div class="col-sm-11" id="descripcion-objeto">
+                            <h6>{{descripcionObjeto}}</h6>
+                        </div>
+                    </div>
 
                 </div>
-
-            </div>
-            <div class="row">
-                <!-- IMAGEN SVG -->
-                <slot></slot>
-            </div>
-
-
-            <div class="row">
-                <div class="col-sm-1" id="avatar">
-                    <img src="/images/myAvatar_adultoMayor.png" alt="Avatar adulto mayor">
+                <!-- columna derecha -->
+                <div class="col-sm-2 col-derecha">
+                    
                 </div>
-
-                <div class="col-sm-11" id="descripcion-objeto">
-                    <h6>{{descripcionObjeto}}</h6>
-                </div>
-            </div>
-
-        </div>
-        <!-- columna derecha -->
-        <div class="col-sm-2 col-derecha">
-
-        </div>
-        <!--fin row-->
+            </div> <!-- fin fila de contenido central y barra lateral derecha -->
+        </div> <!--fin columna contenido central y barra lateral derecha-->
+        
+        
     </div>
 </div>
 
---
+++ assets/js/components/modulo-panel-derecho.component.js
@@ -0,0 +1,37 @@
+parasails.registerComponent('modulo-panel-derecho', {
+    props: {
+
+
+    },
+    data() {
+        return {
+        }
+            
+    },
+    template: `  
+    <div class="container-fluid">
+        <div class="row usuario">
+
+        </div>
+        <div class="row progreso">
+
+        </div>
+        <div class="row enlaces">
+
+        </div>
+        <div class="row usuarios-conenctados">
+
+        </div>
+        <div class="row soporte">
+
+        </div>
+    </div>`,
+    methods: {
+     
+
+    },
+    computed: {
+
+    }
+
+});

072cc8331e678867a63a6818c3779a5e71d1ff67
pedro cuasqui <pedro.cuasqui@gmail.com>
2019-05-08 10:43:15 -0500

    se inserta el archivo .htmlhintrc para configurar las reglas de htmlhint

---
+++ api/controllers/administrador/view-administrar-indice.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+
+  friendlyName: 'View administrar indice',
+
+
+  description: 'Display "Administrar indice" page.',
+
+
+  exits: {
+
+    success: {
+      viewTemplatePath: 'pages/administrador/administrar-indice'
+    }
+
+  },
+
+
+  fn: async function () {
+
+    // Respond with view.
+    return {};
+
+  }
+
+
+};

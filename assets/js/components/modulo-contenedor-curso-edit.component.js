be5b638f966421eee62ccc6b963b3fed9a54e5d7
pedro cuasqui <pedro.cuasqui@gmail.com>
2019-05-26 03:35:46 -0500

    NO SE CONTINÚA CON ESTE ENFOQUE PORQUE LA ACTUALIZACIÓN DE CAMPOS DE LA RAIZ AL COMPONENTE ES MAS TEDIOSA

--- assets/js/components/modulo-contenedor-curso-edit.component.js
+++ assets/js/components/modulo-contenedor-curso-edit.component.js
@@ -1,3 +1,12 @@
+// //////////////////////////////////////////////////
+// //////////////////////////////////////////////////
+// //////////////////////////////////////////////////
+// //////////////////////////////////////////////////
+// //////////////////////////////////////////////////
+// //////////////////////////////////////////////////
+// //////////////////////////////////////////////////
+// ESTE COMPONENTE SE USA SOLO PARA LA EDICION DE UN OBJETO,(MODULO, SUBMODULO )
+
 parasails.registerComponent('modulo-contenedor-curso', {
     props: {
         // tituloContenido: String,
@@ -24,11 +33,11 @@ parasails.registerComponent('modulo-contenedor-curso', {
             type: Object,
 
         },
-        tituloTemporal: {
-            type: String,
-            required: false,
-            default: () => { return '' }
-        },
+        // tituloTemporal: { //SOLO SE USA PARA LA CREACION DE UN MODULO O SUBMODULO
+        //     type: String,
+        //     required: false,
+        //     default: () => { return '' }
+        // },
         usuario: {
             type: Object,
             default: () => { return { nombre: 'Admin', rol: 'Administrador' } }
@@ -43,11 +52,12 @@ parasails.registerComponent('modulo-contenedor-curso', {
     mounted() {
 
         this.campoNombre = Object.keys(this.objetoSeleccionado)[0];
-        this.nombre = this.objetoSeleccionado[this.campoNombre];
+        this.nombre = this.objetoSeleccionado[this.campoNombre];// EL campo objetoSeleccionado.nombreModulo o nombreSubmodulo, según lo envíado
     },
     template: //html
         `  
 <div class="div-contenido container-fluid" v-cloak>
+     <!--CABECERA DE LA VENTANA--> 
     <div class="row" id="div-cabecera"  >
         <div class="col-sm-10">
             <modulo-barra-nav :breadcrumb="breadcrumb"></modulo-barra-nav> 
@@ -57,31 +67,53 @@ parasails.registerComponent('modulo-contenedor-curso', {
         </div>
     </div>
 
-    <!-- Primera fila -Titulo del contenido -->
+    <!--CUERPO DE LA VENTANA-->
     <div class="row" id="div-body">
-        <!-- columna izquierda -->
+        <!-- columna barra de navegacion-->
         <div class="col-sm-2 col-izquierda">
             <modulo-side-var-menu :curso="curso" :objeto-seleccionado="objetoSeleccionado"></modulo-side-var-menu>
         </div>
+        <!-- columna central y lateral derecha del cuerpo -->
         <div class="col-sm-10" id="columna-contenido-lateral">
             <div class="row fila-principal">
-                <!-- Columna central -->
+                <!-- Columna central del cuerpo-->
                 <div class="col-sm-10 col-central">
+                    <!--botones de navegacion y Titulo-->
                     <div class="row">
+                        <!--siguiente-->
                         <div class="navegacion-siguiente">
                             <a :href="navegarSiguiente"><i class="fas fa-arrow-alt-circle-right fas-3x"></i> </a>
                         </div>
+                        <!--atras-->
                         <div class="navegacion-atras">
                             <a :href="navegarAtras"> <i class="fas fa-arrow-alt-circle-left fas-lg"></i> </a>
                         </div>
-                        <div class="col" id="titulo-modulo">
-                            
-                        <h2 v-if="tituloTemporal!=''">{{ tituloTemporal}}</h2>
-                        <h2 v-else>{{ nombre}}</h2>
-
+                        <!--titulo-->
+                        <div class="col" id="titulo-modulo">                          
+                                <div v-else class="form-group">
+                                <!-- <label for="nombreModulo">* Título del módulo</label> -->
+                                    <div class="row">
+                                        <div class="col">
+                                            <input v-if="editarNombre"  
+                                            type="text" 
+                                            id="nombreModulo" 
+                                            name="nombreModulo" 
+                                            v-model="nombreModulo"
+                                            placeholder="Ej: Modulo 1: la computadora"
+                                            :class="[formErrors.nombreModulo ? 'is-invalid' : '']" 
+                                            autofocus>
+                                            <h2 v-else>{{ nombre}}</h2>
+                                            <a v-if="editarNombre" @click="actualizarContenido" title="Guardar Nombre"><i class="fas fa-save"></i></a>
+                                            <a v-else @click="mostrarEditarNombre" ><i class="fas fa-edit"></i></a>
+                                            <a data-toggle="modal" data-target="#modalConfirmaEliminar"  data-placement="top" title="Eliminar Módulo"><i class="fas fa-trash-alt"></i></a>
+                                        </div> <!--col-->
+                                    </div><!--row-->
+                                    
+                                    <div class="invalid-feedback" v-if="formErrors.nombreModulo">Ingrese un nombre para el módulo.</div>
+                                </div>
                         </div>
                     </div>
-
+                    <!--contenido principal-->
                     <div class="row">
                             
                                 <div class="contenedor-slot-principal">
@@ -90,7 +122,7 @@ parasails.registerComponent('modulo-contenedor-curso', {
                             
                     </div>
 
-
+                    <!--Contenido pie de página-->
                     <div class="row pie-contenido">
                         <div class="col-sm-1" id="avatar">
                             <img src="/images/svg/buho_original_1.svg" alt="Avatar adulto mayor">
@@ -120,6 +152,9 @@ parasails.registerComponent('modulo-contenedor-curso', {
 
     },
     computed: {
-
+        permisoDeAdministrador() { //esto parece obvio pero es necesario para los permisos de las politicas
+            let usuarioTienePermiso = this.usuario.rol == 'Administrador';
+            return usuarioTienePermiso;
+        }
     }
 });

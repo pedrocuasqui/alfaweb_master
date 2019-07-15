609b47e495168a7fad8190f78cd5239ddc960d3d
pedro cuasqui <pedro.cuasqui@gmail.com>
2019-07-15 16:06:57 -0500

    se actualiza la fecha del utlimo acceso en interfaz administrar-home

--- assets/js/pages/administrador/administrar-home.page.js
+++ assets/js/pages/administrador/administrar-home.page.js
@@ -3,158 +3,166 @@ parasails.registerPage('administrar-home', {
   //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
   //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
   data: {
-    breadcrumb: [{ id: '', texto: 'indice', enlace: '#' },  ],
+    breadcrumb: [{ id: '', texto: 'indice', enlace: '#' },],
     // cursos:Object, //esta variable será sobreescrita con el contenido de Windows.SAILS_LOCALS.cursos //vue no reconoce la variable cuando no está declarada
-    cursoEliminar:{
-      nombre:'',
-      id:''
+    cursoEliminar: {
+      nombre: '',
+      id: ''
     },
-    
-    editarCurso:false,
-    formErrors:{},
+
+    editarCurso: false,
+    formErrors: {},
     cursos: Object,
-    estudiantes:null,
-    usuario:null,
+    estudiantes: null,
+    usuario: null,
   },
 
   //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
   //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
   //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
-  beforeMount: function() {
+  beforeMount: function () {
     // Attach any initial data from the server.
     _.extend(this, SAILS_LOCALS);
     // console.log(window.SAILS_LOCALS);
-    this.cursos=SAILS_LOCALS.cursos;
-    this.estudiantes= SAILS_LOCALS.estudiantes;
-    this.usuario=SAILS_LOCALS.usuario;
+    this.cursos = SAILS_LOCALS.cursos;
+    this.estudiantes = SAILS_LOCALS.estudiantes;
+    this.usuario = SAILS_LOCALS.usuario;
   },
-  mounted: async function() {
+  mounted: async function () {
 
     // this.asignarEventoClick();
 
-    
-    
+
+
   },
   //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
   //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
   //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
   methods: {
-    asignarEventoClick(){
+    asignarEventoClick() {
 
-      var _this= this;
-      this.cursos.forEach(element=>{
+      var _this = this;
+      this.cursos.forEach(element => {
         console.log('iteracion de elemento');
         console.log(element.id);
-        document.getElementById(element.id).addEventListener("click", abrir=function (){_this.abrirCurso(element.id);},false);
+        document.getElementById(element.id).addEventListener("click", abrir = function () { _this.abrirCurso(element.id); }, false);
       })
-      
-  
+
+
       // elementosBtn.forEach(elemento =>{
       //   elemento.addEventListener("click", this.abrirCurso);
       // });
-      
+
     },
-    seleccionaCursoEliminar(curso){
+    seleccionaCursoEliminar(curso) {
       console.log('seleccionaCursoEliminar');
-      this.cursoEliminar=curso;
+      this.cursoEliminar = curso;
       $(function () {
         $('#modalConfirmaEliminar').modal('show');
       });
     },
-    eliminarDocumento(){
-      var _this=this;
+    eliminarDocumento() {
+      var _this = this;
       axios.get('/eliminar-curso', {
         params: {
           cursoId: this.cursoEliminar.id,
         }
       })
-      .then(function (response) {
-        console.log("respuesta de eliminacion\n"+response);          
-        _this.consultarCursos();
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+        .then(function (response) {
+          console.log("respuesta de eliminacion\n" + response);
+          _this.consultarCursos();
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
 
     },
 
-    consultarCursos(){
-      var _this=this;
+    consultarCursos() {
+      var _this = this;
       axios.get('/consulta-cursos') //llamada a la ruta curso por defecto
-      .then(function (response) {
-        console.log('archivos encontrados');
-        _this.cursos=response.data;
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+        .then(function (response) {
+          console.log('archivos encontrados');
+          _this.cursos = response.data;
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
     },
-    abrirCurso (cursoId){
-      window.location.href='/administrar-indice/?cursoId='+cursoId;
+    abrirCurso(cursoId) {
+      window.location.href = '/administrar-indice/?cursoId=' + cursoId;
 
     },
-    mostrarEditarCurso(cursoId){
+    mostrarEditarCurso(cursoId) {
       // event.cancelBubble = true;
       // if(event.stopPropagation) event.stopPropagation();
-   
-    
+
+
 
     },
-    validarCampos(curso){
-      this.formErrors= {};
+    validarCampos(curso) {
+      this.formErrors = {};
 
-      if(curso.nombre==''){
-        this.formErrors.nombre= true;
+      if (curso.nombre == '') {
+        this.formErrors.nombre = true;
       }
-      if(curso.descripcion==''){
-        this.formErrors.descripcion= true;
+      if (curso.descripcion == '') {
+        this.formErrors.descripcion = true;
       }
 
-      if(Object.keys(this.formErrors).length>0){
+      if (Object.keys(this.formErrors).length > 0) {
         return false;
-      }else{
-        this.guardarCurso(curso);  
+      } else {
+        this.guardarCurso(curso);
       }
 
-      
+
     },
-    guardarCurso(curso){
-      var _this= this;
+    guardarCurso(curso) {
+      var _this = this;
       console.log('guardar curso');
-      this.editarCurso= false;
+      this.editarCurso = false;
 
       // PROCESO PARA GUARDAR EL CURSO
-      var formData= new FormData();
-      formData.append('cursoId',curso.id);
-      formData.append('nombreCurso',curso.nombre);
+      var formData = new FormData();
+      formData.append('cursoId', curso.id);
+      formData.append('nombreCurso', curso.nombre);
       formData.append('descripcionCurso', curso.descripcion);
       axios({
-        method:'post',
-        url:'/actualizar-curso',
-        data:formData
+        method: 'post',
+        url: '/actualizar-curso',
+        data: formData
       })
-      .then(
-        (response)=>{
-          console.log('curso modificado');
-          console.log(response.data)
-        }
+        .then(
+          (response) => {
+            console.log('curso modificado');
+            console.log(response.data)
+          }
         )
-      .catch(
-        (err)=>{
-          console.log('error devuelto');
-          console.log(err)
-        }
-      );
+        .catch(
+          (err) => {
+            console.log('error devuelto');
+            console.log(err)
+          }
+        );
 
       // finalmente habilitar el evento click nuevamente
-      document.getElementById(curso.id).addEventListener("click",abrir=function(){_this.abrirCurso(curso.id)},false);
+      document.getElementById(curso.id).addEventListener("click", abrir = function () { _this.abrirCurso(curso.id) }, false);
     },
-
+    fechaUltimoAccesoEstudiante(estudiante) {
+      let fechaUltimoAcceso = '01-01-1970';
+      fechaUltimoAcceso = new Date(estudiante.updatedAt);
+      // let fecha= fechaUltimoAcceso.toString();
+      let fecha= fechaUltimoAcceso.getDate()+"/"+fechaUltimoAcceso.getMonth()+"/"+fechaUltimoAcceso.getFullYear();
+      
+      // return fecha.substring(1, 12);
+      return fecha
+    }
 
 
   },
-  computed:{
+  computed: {
 
   }
 });

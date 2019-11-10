eb60d919b2488b353c0c1af2c3f6d1bbc057d410
pedro cuasqui <pedro.cuasqui@gmail.com>
2019-11-10 10:12:46 -0500

    boton de usuario en todas interfaces

--- assets/js/components/modulo-contenedor-admin.component.js
+++ assets/js/components/modulo-contenedor-admin.component.js
@@ -19,21 +19,19 @@ parasails.registerComponent("modulo-contenedor-admin", {
   },
   // la definicion de clases css para este componente se encuentra en layout-admin.less
   template://html
-   `  
+    `  
 <div class="div-contenido container-fluid" v-cloak>
     <!-- Barra de navegacion en la parte superior-->
     <div class="row" id="div-cabecera"  >
-        <div class="col-sm-10">
+        <div class="col-sm-8">
             <modulo-barra-nav :breadcrumb="breadcrumb"></modulo-barra-nav> 
         </div>
         <div class="col-sm-2">
             <img src="/images/svg/iconoPolhibou.svg" alt="Logo Polhibou"  />
         </div>
-    </div>
-
-    <!--Contenido -->
-    <div class="row" id="div-body">
-        <div v-if="usuario" class="nombre-usuario">      
+        <div class="col-sm-2">
+             
+            <div v-if="usuario" class="nombre-usuario">      
             <div class="btn-group">
                 <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 
@@ -45,15 +43,21 @@ parasails.registerComponent("modulo-contenedor-admin", {
                     <a v-if="usuario.nombre !='Visitante'" class="dropdown-item" href="/logout">Cerrar sesión</a>
                 </div>
             </div>
-     
+    
         </div>
 
 
         <div v-else class="nombre-usuario">
             <a href="/view-login">Inicia Sesión</a> | <a href="/view-registro-usuario">Regístrate</a> 
         </div>
+
+        </div>
+    </div>
+
+    <!--Contenido -->
+    <div class="row" id="div-body">
         <div class="col contenedor-admin-login">
-            <slot></slot>  
+          <slot></slot>  
         </div>   
     </div>
 
@@ -61,7 +65,7 @@ parasails.registerComponent("modulo-contenedor-admin", {
 
     `,
 
-  data: function() {
+  data: function () {
     return {};
   },
   methods: {}

b30e8ec738b2f70d870e0655223624870f62e5bf
pedro cuasqui <pedro.cuasqui@gmail.com>
2019-11-11 17:28:23 -0500

    actualiza modulo contenedor admin e inicio

---
+++ assets/js/components/modulo-cabecera-general.js
@@ -0,0 +1,61 @@
+parasails.registerComponent('modulo-cabecera-general', {
+	props: {
+		breadcrumb: {
+			type: Array,
+			required: true,
+			default: () => {
+				return [{ nombreModulo: '', id: 1, enlace: '' }]
+			},
+		},
+		usuario: {
+			type: Object,
+			required: false,
+			default: () => {
+				return { nombre: 'Visitante', id: 1, rol: 'Estudiante' }
+			},
+		},
+	},
+	data() {
+		return {}
+	},
+	beforeMount() {},
+	mounted() {},
+
+	template: /*template */ `  
+	<div class="row" id="div-cabecera">
+		<div class="col-sm-6">
+			<modulo-barra-nav :breadcrumb="breadcrumb"></modulo-barra-nav>
+		</div>
+		<div class="col-sm-2">
+			<img src="/images/svg/iconoPolhibou.svg" alt="Logo Polhibou" />
+		</div>
+		<div class="col-sm-4">
+			<template v-if="usuario">
+				<div class="btn-group">
+					<button type="button" class="btn btn-primary dropdown-toggle boton_formulario" data-toggle="dropdown"
+						aria-haspopup="true" aria-expanded="false">
+						<i class="fas fa-user-circle"></i> {{usuario.nombre}} <i class="fas fa-bars"></i>
+					</button>
+					<div class="dropdown-menu">
+						<a class="dropdown-item" href="/view-actualizar-usuario">Cambiar contraseña</a>
+						<div class="dropdown-divider"></div>
+						<a v-if="usuario.nombre !='Visitante'" class="dropdown-item" href="/logout">Cerrar sesión</a>
+					</div>
+				</div>
+			</template>
+			<div v-else>
+				<div class="row">
+					<div class="col">
+						<a class="btn btn-primary boton_formulario" role="button" href="/view-login">Inicia Sesión</a>
+					</div>
+					<div class="col">
+						<a class="btn btn-primary boton_formulario" role="button" href="/view-registro-usuario">Regístrate</a>
+					</div>
+				</div>
+			</div>
+
+		</div>
+</div>`,
+	methods: {},
+	computed: {},
+})
--- assets/js/components/modulo-contenedor-admin.component.js
+++ assets/js/components/modulo-contenedor-admin.component.js
@@ -22,47 +22,8 @@ parasails.registerComponent('modulo-contenedor-admin', {
 	template: /*html*/ `
 <div class="div-contenido container-fluid" v-cloak>
     <!-- Barra de navegacion en la parte superior-->
-    <div class="row" id="div-cabecera" >
-        <div class="col-sm-8">
-            <modulo-barra-nav :breadcrumb="breadcrumb"></modulo-barra-nav>
-        </div>
-        <div class="col-sm-2">
-            <img src="/images/svg/iconoPolhibou.svg" alt="Logo Polhibou"  />
-        </div>
-        <div class="col-sm-2">
-
-            <div v-if="usuario" class="nombre-usuario">
-            <div class="btn-group">
-                <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-
-                    <i class="fas fa-user-circle"></i> {{usuario.nombre}} <i class="fas fa-bars"></i>
-                </button>
-                <div class="dropdown-menu">
-                    <a class="dropdown-item" href="/view-actualizar-usuario">Cambiar contraseña</a>
-                    <div class="dropdown-divider"></div>
-                    <a v-if="usuario.nombre !='Visitante'" class="dropdown-item" href="/logout">Cerrar sesión</a>
-                </div>
-            </div>
-
-        </div>
-
-
-        <div v-else>
-          <div class="row">
-            <div class="col">
-              <a class="btn btn-primary boton_formulario" role="button" href="/view-login">Inicia Sesión</a>
-            </div>
-            <div class="col">
-              <a class="btn btn-primary boton_formulario" role="button" href="/view-registro-usuario">Regístrate</a>
-            </div>
-          </div>
-
-        </div>
-
-
-
-        </div>
-    </div>
+    <!--contiene a div class="row"-->
+    <modulo-cabecera-general :breadcrumb="breadcrumb" :usuario="usuario"></modulo-cabecera-general>
 
     <!--Contenido -->
     <div class="row" id="div-body">

61103e8be134f1870fb65fbe3e9b63921d7af8b8
pedro cuasqui <pedro.cuasqui@gmail.com>
2019-12-08 16:20:14 -0500

    comentario

--- assets/js/pages/autenticacion/registro-usuario.page.js
+++ assets/js/pages/autenticacion/registro-usuario.page.js
@@ -91,7 +91,7 @@ parasails.registerPage("registro-usuario", {
 						title: `Usuario creado correctamente!`,
 						icon: "success",
 						type: "success",
-						text: `El usuario \"${response.data.usuarioCreado.nombre}\" con alias  \"${response.data.usuarioCreado.alias}\" ha sido creado correctamente. Para poder ingresar, confirma tu cuenta en tu correo electrónico\"${response.data.usuarioCreado.email}\"`, // html: `<p><span>El usuario </span><em>${response.data.usuarioCreado.nombre}</em> ha sido creado correctamente</p>`,
+						text: `El usuario \"${response.data.usuarioCreado.nombre}\" con alias  \"${response.data.usuarioCreado.alias}\" ha sido creado correctamente. \nPara poder ingresar, confirma tu cuenta en tu correo electrónico\"${response.data.usuarioCreado.email}\"`, // html: `<p><span>El usuario </span><em>${response.data.usuarioCreado.nombre}</em> ha sido creado correctamente</p>`,
 						// html: "<div>hola</div>",
 
 						confirmButtonClass: "btn btn-success btn-fill",

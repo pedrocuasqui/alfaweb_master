3338875f6682535c2b29de6a890a2e8e9c6688a3
pedro cuasqui <pedro.cuasqui@gmail.com>
2019-12-21 19:06:04 -0500

    creacion de conexion con connect-mongo y tabla session para almacenar sessiones, tener en cuenta que solo funciona con la version 1.1 de connect, tal como indica sails en su documntacion

--- config/session.js
+++ config/session.js
@@ -10,30 +10,38 @@
  */
 
 module.exports.session = {
+	/***************************************************************************
+	 *                                                                          *
+	 * Session secret is automatically generated when your new app is created   *
+	 * Replace at your own risk in production-- you will invalidate the cookies *
+	 * of your users, forcing them to log in again.                             *
+	 *                                                                          *
+	 ***************************************************************************/
+	secret: "c43a3692ca74e5da2d379cf083666115",
+	rememberMeCookieMaxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
 
-  /***************************************************************************
-  *                                                                          *
-  * Session secret is automatically generated when your new app is created   *
-  * Replace at your own risk in production-- you will invalidate the cookies *
-  * of your users, forcing them to log in again.                             *
-  *                                                                          *
-  ***************************************************************************/
-  secret: 'c43a3692ca74e5da2d379cf083666115',
-  rememberMeCookieMaxAge: 30*24*60*60*1000, // 30 days
+	/***************************************************************************
+	 *                                                                          *
+	 * Customize when built-in session support will be skipped.                 *
+	 *                                                                          *
+	 * (Useful for performance tuning; particularly to avoid wasting cycles on  *
+	 * session management when responding to simple requests for static assets, *
+	 * like images or stylesheets.)                                             *
+	 *                                                                          *
+	 * https://sailsjs.com/config/session                                       *
+	 *                                                                          *
+	 ***************************************************************************/
+	// isSessionDisabled: function (req){
+	//   return !!req.path.match(req._sails.LOOKS_LIKE_ASSET_RX);
+	// },
+	adapter: "connect-mongo",
+	// secret: "c43a3692ca74e5da2d379cf083666115",
+	url: "mongodb://localhost:27017/alfabetizaweb",
+	collection: "sessions",
+	auto_reconnect: false,
+	ssl: false,
+	stringify: true
 
-  /***************************************************************************
-  *                                                                          *
-  * Customize when built-in session support will be skipped.                 *
-  *                                                                          *
-  * (Useful for performance tuning; particularly to avoid wasting cycles on  *
-  * session management when responding to simple requests for static assets, *
-  * like images or stylesheets.)                                             *
-  *                                                                          *
-  * https://sailsjs.com/config/session                                       *
-  *                                                                          *
-  ***************************************************************************/
-  // isSessionDisabled: function (req){
-  //   return !!req.path.match(req._sails.LOOKS_LIKE_ASSET_RX);
-  // },
-
+	// adapter: 'sails-mongo',
+	// url: 'mongodb://localhost:27017/alfabetizaweb'
 };

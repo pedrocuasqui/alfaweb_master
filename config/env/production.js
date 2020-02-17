82cf6a55f2a72f7292b599a51c0287cb151cf76a
pedro cuasqui <pedro.cuasqui@gmail.com>
2020-02-17 04:43:46 -0500

    Habilita replicateSet y authSource con ssl true

--- config/env/production.js
+++ config/env/production.js
@@ -71,9 +71,9 @@ module.exports = {
 			// url: "mongodb://localhost:27017/alfabetizaweb",
 			url: process.env.DATABASE_URL,
 			// url:				"mongodb://admin:admin@alfabetizaweb-shard-00-00-cyg3m.mongodb.net:27017,alfabetizaweb-shard-00-01-cyg3m.mongodb.net:27017,alfabetizaweb-shard-00-02-cyg3m.mongodb.net:27017/alfabetizaweb",
-			ssl: true
-			// replicaSet: "alfabetizaweb-shard-0",
-			// authSource: "admin"
+			ssl: true,
+			replicaSet: "alfabetizaweb-shard-0",
+			authSource: "admin"
 		}
 	},
 
